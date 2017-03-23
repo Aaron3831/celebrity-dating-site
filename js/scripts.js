9b2f920a4f118fc4b2ce81b76fdfb1f1af1bc668
Aaron Nguyen and LaTanya Donaldson <aaron@>
2017-03-23 14:57:40 -0700

    Create some more results using if, else if, and else.

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,21 @@ $(document).ready(function(){
     var location = $("select#location").val();
     var gender = $("select#gender").val();
 
-    if (gender === '3' || '1') {
+    if (gender === '3' && age === 29) {
     $(".justin").show();
     $(".diaz").hide();
+    $(".ryan").hide();
     }
-
+    else if (location === 'Not from America' || age === 30 ) {
+    $(".ryan").show();
+    $(".justin").hide();
+    $(".diaz").hide();
+    }
     else {
       (gender === 'male')
     $(".justin").hide();
     $(".diaz").show();
+    $(".ryan").hide();
     }
 });
 });
